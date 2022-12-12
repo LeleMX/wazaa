df408d292aee0c02c268c531e5c46a0898e897fb
LeleMX <65337259+lelemx@users.noreply.github.com>
2022-12-12 12:07:42 -0700

    HICE CAMBIOS

--- EXAMEN3U/src/Sistema.java
+++ EXAMEN3U/src/Sistema.java
@@ -43,43 +43,140 @@ public class Sistema {
         System.out.println("Indique el tipo de cuenta que tiene");
         System.out.println("(1) Cuenta de ahorro");
         System.out.println("(2) Cuenta de inversion");
-        int opcion=t.nextInt();
+        int opcion = t.nextInt();
+        t.nextLine();
         switch (opcion){
             case 1:
                 System.out.println("Para poder realizar su retiro ingrese el nombre del titular");
+                String nombre = t.nextLine();
                 for (int i = 0; i < cuentas.length ; i++) {
-                    String nombre=t.nextLine();
-                    if (cuentas[i].getNombreCliente().equals(nombre) ) {
+                    if (cuentas[i] != null) {
+                        if (cuentas[i].getNombreCliente().equals(nombre) ) {
+                            System.out.println("INGRESE MONTO A RETIRAR");
+                            double monto = t.nextDouble();
+                            double saldoCuenta = cuentas[i].getMonto();
+                            double saldoNuevo = saldoCuenta - monto;
+                            if (saldoNuevo >= 500) {
+                                cuentas[i].setMonto(saldoNuevo);
+                                System.out.println("SE ACTUALIZÓ EL MONTO DE LA CUENTA");
+                                System.out.println("CUENTA: " + cuentas[i].getNombreCliente() +
+                                        " MONTO VIEJO: " + saldoCuenta +
+                                        " MONTO NUEVO: " + saldoNuevo);
+                                break;
+                            } else {
+                                System.out.println("SALDO INSUFICIENTE");
+                                break;
+                            }
+                        }
+                        else {
 
+                        }
                     }
-                }
-                System.out.println("¿que cantidad desea retirar?");
-                double cantidad=t.nextDouble();
-                for (int i = 0; i < cuentas.length ; i++) {
-
-                    if (cuentas[i].getMonto() >=1000) {
-
-
-                    } else if (cuentas[i].getMonto() <= 1000) {
-                        System.out.println("*Saldo insuficiente*");
-
+                    else {
+                        System.out.println("NO SE ENCONTRÓ LA CUENTA");
+                        break;
                     }
                 }
                 break;
             case 2:
-                cuentaInversion();
-                System.out.println("¿que cantidad desea retirar?");
+                System.out.println("Para poder realizar su retiro ingrese el nombre del titular");
+                String nombreCuenta = t.nextLine();
+                for (int i = 0; i < cuentas.length ; i++) {
+                    if (cuentas[i] != null) {
+                        if (cuentas[i].getNombreCliente().equals(nombreCuenta) ) {
+                            System.out.println("INGRESE MONTO A RETIRAR");
+                            double monto = t.nextDouble();
+                            double saldoCuenta = cuentas[i].getMonto();
+                            double saldoNuevo = saldoCuenta - monto;
+                            if (saldoNuevo >= 1000) {
+                                cuentas[i].setMonto(saldoNuevo);
+                                System.out.println("SE ACTUALIZÓ EL MONTO DE LA CUENTA");
+                                System.out.println("CUENTA: " + cuentas[i].getNombreCliente() +
+                                        " MONTO VIEJO: " + saldoCuenta +
+                                        " MONTO NUEVO: " + saldoNuevo);
+                                break;
+                            } else {
+                                System.out.println("SALDO INSUFICIENTE");
+                                break;
+                            }
+                        }
+                        else {
+
+                        }
+                    }
+                    else {
+                        System.out.println("NO SE ENCONTRÓ LA CUENTA");
+                        break;
+                    }
+                }
                 break;
         }
-
-
-
-
-
     }
 
     public static void deposito() {
+        System.out.println("Indique el tipo de cuenta que tiene: ");
+        System.out.println("(1) Cuenta de ahorro");
+        System.out.println("(2) Cuenta de inversion");
+        int opcion = t.nextInt();
+        t.nextLine();
+        switch (opcion){
+            case 1:
+                System.out.println("Para poder realizar su deposito ingrese el nombre del titular");
+                String nombre=t.nextLine();
+                for (int i = 0; i < cuentas.length ; i++) {
+                    if (cuentas[i] != null) {
+                        if (cuentas[i].getNombreCliente().equals(nombre) ) {
+                            System.out.println("INGRESE MONTO A DEPOSITAR");
+                            double monto = t.nextDouble();
+                            double saldoCuenta = cuentas[i].getMonto();
+                            double saldoNuevo = saldoCuenta + monto;
 
+                            cuentas[i].setMonto(saldoNuevo);
+                            System.out.println("SE ACTUALIZÓ EL MONTO DE LA CUENTA");
+                            System.out.println("CUENTA: " + cuentas[i].getNombreCliente() +
+                                    " MONTO VIEJO: " + saldoCuenta +
+                                    " MONTO NUEVO: " + saldoNuevo);
+                            break;
+                        }
+                        else {
+
+                        }
+                    }
+                    else {
+                        System.out.println("NO SE ENCONTRÓ LA CUENTA");
+                        break;
+                    }
+                }
+                break;
+            case 2:
+                System.out.println("Para poder realizar su deposito ingrese el nombre del titular: ");
+                String nombreCuenta = t.nextLine();
+                for (int i = 0; i < cuentas.length ; i++) {
+                    if (cuentas[i] != null) {
+                        if (cuentas[i].getNombreCliente().equals(nombreCuenta) ) {
+                            System.out.println("INGRESE MONTO A DEPOSITAR");
+                            double monto = t.nextDouble();
+                            double saldoCuenta = cuentas[i].getMonto();
+                            double saldoNuevo = saldoCuenta + monto;
+
+                            cuentas[i].setMonto(saldoNuevo);
+                            System.out.println("SE ACTUALIZÓ EL MONTO DE LA CUENTA");
+                            System.out.println("CUENTA: " + cuentas[i].getNombreCliente() +
+                                    " MONTO VIEJO: " + saldoCuenta +
+                                    " MONTO NUEVO: " + saldoNuevo);
+                            break;
+                        }
+                        else {
+
+                        }
+                    }
+                    else {
+                        System.out.println("NO SE ENCONTRÓ LA CUENTA");
+                        break;
+                    }
+                }
+                break;
+        }
     }
 
     private static int buscador() {
